ce8a6f003fafe99f27dadbcd41cf8c14ffb7e43e
David Billups <davidtbillups@gmail.com>
2021-05-19 11:17:19 -0400

    showing matt some stuff

--- TabloidMVC/Controllers/CategoryController.cs
+++ TabloidMVC/Controllers/CategoryController.cs
@@ -43,10 +43,11 @@ namespace TabloidMVC.Controllers
         // POST: CategoryController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Category category)
         {
             try
             {
+                _categoryRepository.Add(category);
                 return RedirectToAction(nameof(Index));
             }
             catch
