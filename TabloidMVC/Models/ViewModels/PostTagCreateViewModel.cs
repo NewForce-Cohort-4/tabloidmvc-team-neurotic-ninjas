ec6d5b7372383e3b7e4c2d66141ac68e9f275cf6
David Billups <davidtbillups@gmail.com>
2021-05-21 09:53:09 -0400

    fix it FRIDAY

---
+++ TabloidMVC/Models/ViewModels/PostTagCreateViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TabloidMVC.Models.ViewModels
+{
+    public class PostTagCreateViewModel
+    {
+        public List<Tags> TagOptions { get; set; }
+        public List<int> TagIds { get; set; }
+        public Post Post { get; set; }
+    }
+}
---
+++ TabloidMVC/Repositories/PostTagRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TabloidMVC.Repositories
+{
+    public class PostTagRepository : BaseRepository, IPostTagRepository
+    {
+        public PostTagRepository(IConfiguration config) : base(config) { }
+
+        public void AddPostTag(int postId, List<int> tagIds)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
